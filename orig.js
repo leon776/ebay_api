97903411d1c8c16ee49147f107e2595eef5a0a46
walkerqiao <150643694@qq.com>
2017-06-24 21:49:44 +0800

    update

--- orig.js
+++ orig.js
@@ -1,89 +1,81 @@
+require('es6-promise').polyfill()
+require('isomorphic-fetch')
 let fs = require('fs')
 let request = require('request')
 
-/**
- * @desc 获取ebay api搜索结果，该版本写csv文件
- *
- * 搜索的关键词、开始时间、结束时间、价格暂时在变量中提供
- */
-
 let outputfile = './output.csv'
-
-/**
- * @desc 根据页面数、最低价、关键词、开始日期、结束日期产生请求API的URL
- * @return string url
- */
 function makeURL(page_number, on_going, min_price, query_string, fromdate, todate, app_name) {
-  var urlfilter = "";
-  var filterarray = [{
-      "name": "MinPrice",
-      "value": min_price == null ? 0 : min_price,
-      "paramName": "Currency",
-      "paramValue": "USD"
-    }, // CNY
-    {
-      "name": "FreeShippingOnly",
-      "value": "false",
-      "paramName": "",
-      "paramValue": ""
-    }, {
-      "name": "SoldItemsOnly",
-      "value": "true",
-      "paramName": "",
-      "paramValue": ""
-    }, {
-      "name": "EndTimeFrom",
-      "value": fromdate,
-      "paramName": "",
-      "paramValue": ""
-    }, {
-      "name": "EndTimeTo",
-      "value": todate,
-      "paramName": "",
-      "paramValue": ""
-    }
-
-  ];
-
-  // Iterate through each filter in the array
-  for (var i = 0; i < filterarray.length; i++) {
-    //Index each item filter in filterarray
-    var itemfilter = filterarray[i];
-    // Iterate through each parameter in each item filter
-    for (var index in itemfilter) {
-      // Check to see if the paramter has a value (some don't)
-      if (itemfilter[index] !== "") {
-        if (itemfilter[index] instanceof Array) {
-          for (var r = 0; r < itemfilter[index].length; r++) {
-            var value = itemfilter[index][r];
-            urlfilter += "&itemFilter\(" + i + "\)." + index + "\(" + r + "\)=" + value;
-          }
-        } else {
-          urlfilter += "&itemFilter\(" + i + "\)." + index + "=" + itemfilter[index];
+    var urlfilter = "";
+    var filterarray = [{
+            "name": "MinPrice",
+            "value": min_price == null ? 0 : min_price,
+            "paramName": "Currency",
+            "paramValue": "USD"
+        }, // CNY
+        {
+            "name": "FreeShippingOnly",
+            "value": "false",
+            "paramName": "",
+            "paramValue": ""
+        }, {
+            "name": "SoldItemsOnly",
+            "value": "true",
+            "paramName": "",
+            "paramValue": ""
+        }, {
+            "name": "EndTimeFrom",
+            "value": fromdate,
+            "paramName": "",
+            "paramValue": ""
+        }, {
+            "name": "EndTimeTo",
+            "value": todate,
+            "paramName": "",
+            "paramValue": ""
         }
-      }
+
+    ];
+
+    // Iterate through each filter in the array
+    for (var i = 0; i < filterarray.length; i++) {
+        //Index each item filter in filterarray
+        var itemfilter = filterarray[i];
+        // Iterate through each parameter in each item filter
+        for (var index in itemfilter) {
+            // Check to see if the paramter has a value (some don't)
+            if (itemfilter[index] !== "") {
+                if (itemfilter[index] instanceof Array) {
+                    for (var r = 0; r < itemfilter[index].length; r++) {
+                        var value = itemfilter[index][r];
+                        urlfilter += "&itemFilter\(" + i + "\)." + index + "\(" + r + "\)=" + value;
+                    }
+                } else {
+                    urlfilter += "&itemFilter\(" + i + "\)." + index + "=" + itemfilter[index];
+                }
+            }
+        }
     }
-  }
-  //davidkwa-bidtify-PRD-645f30466-13f9307d
-  // Construct the request
-  // Replace MyAppID with your Production AppID
-  var url = "http://svcs.ebay.com/services/search/FindingService/v1";
-  if (!on_going)
-    url += "?OPERATION-NAME=findCompletedItems";
-  else
-    url += "?OPERATION-NAME=findItemsByKeywords";
-  url += "&SERVICE-VERSION=1.0.0";
-  url += "&SECURITY-APPNAME=" + app_name;
-  url += "&GLOBAL-ID=EBAY-US";
-  url += "&RESPONSE-DATA-FORMAT=JSON";
-  //url += "&callback=_cb_findItemsByKeywords";
-  url += "&REST-PAYLOAD";
-  url += "&keywords="; //Adidas%20yeezy%20v2%20core%20black%20white%20size%2010
-  url += encodeURIComponent(query_string);
-  url += "&paginationInput.pageNumber=" + page_number;
-  url += "&paginationInput.entriesPerPage=100";
-  url += urlfilter;
-  return url;
+    //davidkwa-bidtify-PRD-645f30466-13f9307d
+    // Construct the request
+    // Replace MyAppID with your Production AppID
+    var url = "http://svcs.ebay.com/services/search/FindingService/v1";
+    if (!on_going)
+        url += "?OPERATION-NAME=findCompletedItems";
+    else
+        url += "?OPERATION-NAME=findItemsByKeywords";
+    url += "&SERVICE-VERSION=1.0.0";
+    url += "&SECURITY-APPNAME=" + app_name;
+    url += "&GLOBAL-ID=EBAY-US";
+    url += "&RESPONSE-DATA-FORMAT=JSON";
+    //url += "&callback=_cb_findItemsByKeywords";
+    url += "&REST-PAYLOAD";
+    url += "&keywords="; //Adidas%20yeezy%20v2%20core%20black%20white%20size%2010
+    url += encodeURIComponent(query_string);
+    url += "&paginationInput.pageNumber=" + page_number;
+    url += "&paginationInput.entriesPerPage=100";
+    url += urlfilter;
+
+    return url;
 }
 
 function writeHead(file) {
@@ -172,6 +164,20 @@ function writeRecord(file, record, no) {
 
   fs.appendFileSync(file, rowString + "\n")
 }
+let start = '05 20,2017'
+let end = '06 20,2017'
+
+let page_number = 1
+let on_going = false
+let min_price = 160
+let query_string = 'Jordan 4 pure money'
+let fromdate = (new Date(start)).toISOString()
+let todate = (new Date(end)).toISOString()
+let app_name = 'davidkwa-bidtify-PRD-645f30466-13f9307d'
+
+// 先删除原来文件
+fs.unlinkSync(outputfile)
+writeHead(outputfile)
 function makeRequest(page_number, on_going, min_price, query_string, fromdate, todate, app_name) {
   let url = makeURL(page_number, on_going, min_price, query_string, fromdate, todate, app_name)
   console.log('Begin request :', url)
@@ -219,35 +225,51 @@ function makeRequest(page_number, on_going, min_price, query_string, fromdate, t
     }
   })
 }
+function makeFetch(page_number, on_going, min_price, query_string, fromdate, todate, app_name) {
+  let url = makeURL(page_number, on_going, min_price, query_string, fromdate, todate, app_name)
+  console.log(url)
+  fetch(url).then(
+    response => {
+      return response.json()
+    }
+  ).then(
+    json => {
+      // 首先从响应中拿出各个属性
+      let {
+        findCompletedItemsResponse: [
+          {
+            ack,
+            paginationOutput,
+            searchResult,
+            timestamp,
+            version,
+          }
+        ]
+      } = json
 
+      let [resStatus] = ack
 
-// 开始时间
-let start = '05 20,2017'
+      if(resStatus === 'Success') { // 请求成功
+        let count = searchResult[0]['@count'] // 含有特殊符号，直接取出
+        let [
+          { item },
+        ] = searchResult
 
-// 结束时间
-let end = '06 20,2017'
+        item.map(function(record, index){
+          let no = 100 * (page_number - 1) + index + 1
+          writeRecord(outputfile, record, no)
+        })
 
-// 开始页码
-let page_number = 1
+      } else if (resStatus === 'Failure') { // 请求失败
+      }
+    }
+  )
+}
 
-// unkwown
-let on_going = false
-
-// 最低价设置
-let min_price = 160
-
-// 查询关键词
-let query_string = 'Jordan 4 pure money'
-
-let fromdate = (new Date(start)).toISOString()
-let todate = (new Date(end)).toISOString()
-
-// API token
-let app_name = 'davidkwa-bidtify-PRD-645f30466-13f9307d'
-
-// 先删除原来文件
-fs.unlinkSync(outputfile)
-// 写表头
-writeHead(outputfile)
-// 请求数据，写表体
 makeRequest(page_number, on_going, min_price, query_string, fromdate, todate, app_name)
+
+/*while(page_number <= 100) {
+  makeRequest(page_number, on_going, min_price, query_string, fromdate, todate, app_name)
+  page_number++
+}*/
+
